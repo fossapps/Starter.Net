206e3d644c169e88857c05a7fd300e29589843e4
Nishchal Gautam <gautam.nishchal@gmail.com>
2019-04-07 22:46:08 +0700

    feat(TokenFactory): add method to create jwt

--- Src/Starter.Net.Api/Services/TokenFactory.cs
+++ Src/Starter.Net.Api/Services/TokenFactory.cs
@@ -1,10 +1,22 @@
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Starter.Net.Api.Configs;
 
 namespace Starter.Net.Api.Services
 {
     public class TokenFactory : ITokenFactory
     {
+        private readonly JwtBearerOptions _jwt;
+        public TokenFactory(IOptions<Configs.Authentication> authConfig)
+        {
+            _jwt = authConfig.Value.JwtBearerOptions;
+        }
+
         public string GenerateToken(int size)
         {
             var randomNumber = new byte[size];
@@ -13,6 +25,21 @@ namespace Starter.Net.Api.Services
                 rng.GetBytes(randomNumber);
                 return Convert.ToBase64String(randomNumber);
             }
+        }
+
+        public string GenerateJwtToken(ClaimsPrincipal principal)
+        {
+            var key = Encoding.ASCII.GetBytes(_jwt.SigningKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = _jwt.Issuer,
+                Audience = _jwt.Audience,
+                Subject = new ClaimsIdentity(principal.Claims),
+                Expires = DateTime.UtcNow.AddMinutes(_jwt.JwtTtl),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
         }
     }
 }
