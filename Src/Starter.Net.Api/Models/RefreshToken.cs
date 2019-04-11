58820b2b5bf939dfc5ca5b699d32129977d1764d
Nishchal Gautam <gautam.nishchal@gmail.com>
2019-04-11 11:27:40 +0700

    feat(refreshToken): add more info to refresh tokenks

--- Src/Starter.Net.Api/Models/RefreshToken.cs
+++ Src/Starter.Net.Api/Models/RefreshToken.cs
@@ -5,5 +5,8 @@ namespace Starter.Net.Api.Models
         public string Id { set; get; }
         public string User { set; get; }
         public string Value { set; get; }
+        public string Useragent { set; get; }
+        public string IpAddress { set; get; }
+        public string Location { set; get; }
     }
 }
