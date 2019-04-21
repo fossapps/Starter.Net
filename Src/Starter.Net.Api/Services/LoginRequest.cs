c70516e27c7be00c77bd50134b7ffd2b0d80c2c6
Nishchal Gautam <gautam.nishchal@gmail.com>
2019-04-21 16:07:43 +0700

    fix(auth): fix auth endpoint

--- Src/Starter.Net.Api/Services/LoginRequest.cs
+++ Src/Starter.Net.Api/Services/LoginRequest.cs
@@ -1,7 +1,9 @@
 namespace Starter.Net.Api.Services
 {
-    public class LoginRequest: ViewModels.LoginRequest
+    public class LoginRequest
     {
+        public string Login { set; get; }
+        public string Password { set; get; }
         public string UserAgent { set; get; }
         public string IpAddress { set; get; }
         public string Location { set; get; }
